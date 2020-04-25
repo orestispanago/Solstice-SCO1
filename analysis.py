cf066c050d089078f0cf4cc42277ddce9cc67503
orestis <orestispanago@gmail.com>
2020-04-26 00:34:21 +0300

    added auxiliary surface

--- analysis.py
+++ analysis.py
@@ -6,10 +6,10 @@ transversal.run()
 
 df = pd.read_csv("raw/transversal.txt",sep='\s+',names=range(47))
 
-
-angle = df.loc[0::12,3].values
-eff_side1_df = df.loc[9::12, 23].values
-eff_side2_df = df.loc[9::12, 45].values
+step=14 # 12 for plain, 14 for aux surface
+angle = df.loc[0::step,3].values # first_row::step,column
+eff_side1_df = df.loc[9::step, 23].values
+eff_side2_df = df.loc[9::step, 45].values
 
 angle_eff = pd.DataFrame({'side1':eff_side1_df,'side2':eff_side2_df}, index=angle)  # 1st row as the column names
 angle_eff['total_eff'] = angle_eff['side1'] + angle_eff['side2']
